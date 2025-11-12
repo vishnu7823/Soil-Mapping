cec92fdce1da78805545c094486539324244c119
M Vishnu vardhan <vishnuvardhan7823@gmail.com>
2025-11-12 23:41:19 +0530

    project done

--- frontend/src/api/soilService.js
+++ frontend/src/api/soilService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/soil"; // Change to your deployed backend URL later
+const API_URL = "https://soil-mapping.onrender.com/api/soil"; // Change to your deployed backend URL later
 
 export const getStates = () => axios.get(`${API_URL}/states`);
 export const getDistricts = (state) => axios.get(`${API_URL}/districts/${state}`);
